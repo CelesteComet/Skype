ba78a04f32234eb595cbb896f683e94c2d2728bc
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2018-02-02 11:23:47 -0800

    i love edward

--- frontend/components/ModalProfile/ModalProfile.jsx
+++ frontend/components/ModalProfile/ModalProfile.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ProfileItem from '../ProfileItem';
+import CircleImageIcon from '../CircleImageIcon';
 import { toggleProfileModal } from '../../actions/uiActions';
 import { connect } from 'react-redux';
+
 
 class ModalProfile extends Component {
 
@@ -24,15 +26,26 @@ class ModalProfile extends Component {
 
           </header>
           <div className="modal-main">
-            <h3>Bruce Wong</h3>
-            <p>brucewong21</p>
-            <h2>Online</h2>
-            <p>Notifications on</p>
-            <div>
-              <span>Change Activity Message</span>
+            <CircleImageIcon 
+              src='/images/myicon.jpeg'
+              status='Online' />
+            <div className="modal-content">
+              <h3>Bruce Wong</h3>
+              <p>brucewong21</p>
+              <h2>Online<i className="fa fa-sign-out icon-blue" aria-hidden="true"></i></h2>
+              <p className="notifications">Notifications on</p>
+              <div>
+                <span>Change Activity Message</span>
+              </div>
+              <div className="big-buttons first">
+                <i className="fa fa-cog icon-blue"></i>
+                Manage Account
+              </div>
+              <div className="big-buttons">
+                <i className="fa fa-sign-out icon-blue" aria-hidden="true"></i>
+                Sign Out
+              </div>
             </div>
-            <div className="big-buttons">Manage Account</div>
-            <div className="big-buttons">Sign Out</div>
           </div>
         </div>
       </div>
